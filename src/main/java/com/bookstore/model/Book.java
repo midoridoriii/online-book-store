49c22b575323f41181c026a1099a6ba9d23c5eb4
Valeriia Horbatko <valissa069@gmail.com>
2026-08-15 11:58:02 +0300

    Address review comments

--- src/main/java/com/bookstore/model/Book.java
+++ src/main/java/com/bookstore/model/Book.java
@@ -10,13 +10,14 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "books")
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
-@org.hibernate.annotations.SQLRestriction("is_deleted=false")
+@SQLRestriction("is_deleted=false")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
