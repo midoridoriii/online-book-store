d1e1b3a1eb78f724bbf3fe28486035c8b02ee1be
Valeriia Horbatko <valissa069@gmail.com>
2026-08-30 18:07:39 +0300

    Add JWT authentication support

---
+++ src/main/java/com/bookstore/dto/UserLoginRequestDto.java
@@ -0,0 +1,17 @@
+package com.bookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginRequestDto {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
